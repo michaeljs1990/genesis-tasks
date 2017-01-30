758e25167968dd000312d6bea1194edf91f07d26
Michael Schuett <michaeljs1990@gmail.com>
2017-01-29 21:27:31 -0500

    Get Provisioning Config Task
    This lets us pull in any node specific config that may be defined inside
    of evie before be begin.

--- tasks/ProvisionGetConfig.rb
+++ tasks/ProvisionGetConfig.rb
@@ -1,29 +1,13 @@
 class ProvisionGetConfig
   include Genesis::Framework::Task
 
-  precondition "has asset tag?" do
-    not facter['asset_tag'].nil?
-  end
-
-  precondition "asset tag doesn't exist in collins?" do
-    t = facter['asset_tag']
-    log "Checking for #{t} in collins..."
-    exists = collins.exists?(t)
-    log "Asset #{t} not found in collins!" if !exists
-    !exists
-  end
-
   run do
-    begin
-      collins.create!(facter['asset_tag'], :generate_ipmi => true)
-    rescue Collins::RequestError => e
-      if e.code == 409
-        log "Asset %s already exists in collins" % facter['asset_tag']
-      else
-        log "Error trying to create asset in collins. Message: %s" % e.message
-        raise e
-      end
+    config_url = config[:genesis_tasks][:provision_get_config][:config_url] + "/#{facter['asset_tag']}"
+    log "Downloading asset config from #{config_url}"
+    open('/var/run/genesis/provision.json', 'wb') do |file|
+      file << open(config_url).read
     end
   end
+
 end
 
