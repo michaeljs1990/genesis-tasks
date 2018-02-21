1db8f55c94150bb81b51f7495e4ae3872734a036
Michael Schuett <michaeljs1990@gmail.com>
2018-02-21 01:40:55 -0500

    GRUB!!!!
    I hate it so much i should really switch to a different bootloader.

--- tasks/ProvisionGrubSetup.rb
+++ tasks/ProvisionGrubSetup.rb
@@ -8,8 +8,14 @@ class ProvisionGrubSetup
   end
 
   run do
+    rootdevice = '/dev/sda'
+    magic_tic = "'\"'\"'"
     # don't hard code this in the future. Also support more
     # than my basic disk config... that comes latter though.
+    
+    Mixins::Provision.chroot_cmd "sed -i -e #{magic_tic}s/GRUB_HIDDEN_TIMEOUT=.*/# GRUB_HIDDEN_TIMEOUT=0/#{magic_tic} /etc/default/grub"
+    Mixins::Provision.chroot_cmd "sed -i -e #{magic_tic}s/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/#{magic_tic} /etc/default/grub"
+    Mixins::Provision.chroot_cmd "sed -i -e #{magic_tic}s/GRUB_CMDLINE_LINUX_DEFAULT=.*/# GRUB_CMDLINE_LINUX_DEFAULT=\"text\"/#{magic_tic} /etc/default/grub"
     Mixins::Provision.chroot_cmd "grub-install -v /dev/sda"
     Mixins::Provision.chroot_cmd "update-grub"
   end
