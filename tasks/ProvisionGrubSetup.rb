2661893a93b58c5bd6a19e4c97d0b0f343cee18d
Michael Schuett <michaeljs1990@users.noreply.github.com>
2018-02-21 11:21:15 -0500

    Use defined rootdevice
    actually use the variable defined above for it's purpose.

--- tasks/ProvisionGrubSetup.rb
+++ tasks/ProvisionGrubSetup.rb
@@ -16,7 +16,7 @@ class ProvisionGrubSetup
     Mixins::Provision.chroot_cmd "sed -i -e #{magic_tic}s/GRUB_HIDDEN_TIMEOUT=.*/# GRUB_HIDDEN_TIMEOUT=0/#{magic_tic} /etc/default/grub"
     Mixins::Provision.chroot_cmd "sed -i -e #{magic_tic}s/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/#{magic_tic} /etc/default/grub"
     Mixins::Provision.chroot_cmd "sed -i -e #{magic_tic}s/GRUB_CMDLINE_LINUX_DEFAULT=.*/# GRUB_CMDLINE_LINUX_DEFAULT=\"text\"/#{magic_tic} /etc/default/grub"
-    Mixins::Provision.chroot_cmd "grub-install -v /dev/sda"
+    Mixins::Provision.chroot_cmd "grub-install -v #{rootdevice}"
     Mixins::Provision.chroot_cmd "update-grub"
   end
 
