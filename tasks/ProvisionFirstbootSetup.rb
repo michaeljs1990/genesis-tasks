f86e1cb6b7a6e8fc481d271a00f860ea92afcfd3
Michael Schuett <michaeljs1990@gmail.com>
2018-03-04 15:54:14 -0500

    More gem env mess
    The gem environment seems to ignore the .gemrc file when a gem is
    launched even though gem env will show everything as correct. Adding
    in GEM_HOME fixes this... overall this ruby stuff turned into a huge
    mess and will need to be cleaned up after everything is working.

--- tasks/ProvisionFirstbootSetup.rb
+++ tasks/ProvisionFirstbootSetup.rb
@@ -20,12 +20,15 @@ class ProvisionFirstbootSetup
     # ruby when it's inside a chroot but the following is needed in order
     # for librarian-puppet to properly install the modules.... This will
     # get cleaned up later after i start writting the first boot script
-    # and figure out what is going on.
+    # and figure out what is going on. For whatever reason gemhome when
+    # placed in the .gemrc file isn't read when loading gems so I haven't
+    # been able to get this working without manually setting GEM_HOME...
     binary_dir = '/opt/puppetlabs/puppet/bin/'
     librarian = '/opt/puppetlabs/puppet/bin/librarian-puppet'
+    gemhome = 'GEM_HOME=/opt/puppetlabs/puppet/lib/ruby/gems/2.4.0'
     Mixins::Provision.chroot_cmd "rm -rf #{path}"
     Mixins::Provision.chroot_cmd "git clone #{repo} #{path}"
-    Mixins::Provision.chroot_cmd "cd #{path} && PATH=$PATH:/opt/puppetlabs/bin #{librarian} install"
+    Mixins::Provision.chroot_cmd "cd #{path} && #{gemhome} PATH=$PATH:/opt/puppetlabs/bin #{librarian} install"
 
   end
 
