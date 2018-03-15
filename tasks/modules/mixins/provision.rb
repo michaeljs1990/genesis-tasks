17c98100e374deb0ede83c05a8d252e43cba10ad
Michael Schuett <michaeljs1990@gmail.com>
2018-03-14 21:54:15 -0400

    Cleanup crap around setting env
    The path was now being set properly before inside the chroot. Using
    env in the chroot to first clear the environment and then execute the
    command works great and provides a super minimal env.

--- tasks/modules/mixins/provision.rb
+++ tasks/modules/mixins/provision.rb
@@ -17,11 +17,11 @@ module Mixins
 
     def self.chroot_apt_install packages
       pkg = packages.join " "
-      run_cmd("chroot #{CHROOT_PATH} /bin/bash -c 'DEBIAN_FRONTEND=noninteractive apt install -y #{pkg}'")
+      run_cmd("chroot #{CHROOT_PATH} env -i bash -l -c 'DEBIAN_FRONTEND=noninteractive apt install -y #{pkg}'")
     end
 
     def self.chroot_cmd cmd
-      run_cmd("chroot #{CHROOT_PATH} /bin/bash -c '#{cmd}'")
+      run_cmd("chroot #{CHROOT_PATH} env -i bash -l -c '#{cmd}'")
     end
 
   end
